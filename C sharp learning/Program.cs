7e40a9adf49fbb6ba4f414364af56fdf43accf0f
kitsune <57214464+Terr0rize@users.noreply.github.com>
2020-09-02 19:46:57 +0400

    Add files via upload

---
+++ C sharp learning/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic; //массив, списки.
+using System.ComponentModel;
+using System.Globalization;
+using System.Net;
+
+namespace C_sharp_learning
+{
+    class MainClass
+    {
+        static void Main(string[] args) //метод срабатывает при старте программы
+        {
+            Console.WriteLine("Enter number: ");         
+                try
+                {
+                    int num = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Num is: " + num);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("FormatException: " + ex);
+                }finally
+                  {
+                Console.WriteLine("Try catch is exit");
+                  }
+            
+            Console.ReadKey();
+        }
+    }
+}
